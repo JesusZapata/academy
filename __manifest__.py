c2d7beb7d7cf855cf22c44e9691df46bae333aef
Jesus Zapata <jesus@vauxoo.com>
2016-12-22 08:44:13 +0000

    [IMP] openacademy: Anexando el modulo de website_sale y personalizando

--- __manifest__.py
+++ __manifest__.py
@@ -20,13 +20,14 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['website', 'mail'],
+    'depends': ['website', 'website_sale'],
 
     # always loaded
     'data': [
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+	'data.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
